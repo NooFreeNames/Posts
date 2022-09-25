52d314d66fd6b25df7842c92aa9710bfec725fba
FreezeGamer96 <Nonexistent@email.non>
2022-09-25 15:58:42 +0500

    Added a button to upload an image for a post

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const upload_image_button = document.querySelector('.upload-image-button');
+const upload_image_name = document.querySelector('.upload-text')
+const image_input = document.querySelector('input[id=upload-image]')
+let click_redirection = () => {
+    image_input.click();
+}
+let change_handler = () => {
+    if (image_input.value) {
+
+        let fileName = image_input.value.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
+
+        if (fileName.length > 22) {
+            fileName = fileName.slice(0, 21) + '...';
+        }
+
+        upload_image_name.textContent = fileName;
+
+    } else {
+        upload_image_name.textContent = "Файл не выбран";
+    }
+}
+upload_image_button.addEventListener('click', click_redirection);
+image_input.addEventListener('change', change_handler);
--- templates/main.php
+++ templates/main.php
@@ -4,20 +4,32 @@
     <!--    </a>-->
     <ul class="post-list">
         <li class="post-container">
-            <form action="post_addition.php" method="post">
-                <input class="post-input-field title-input-field title-font block" type="text" placeholder="Заголовок" name="title" maxlength="45"/>
-                <p>
-                    <textarea class="post-input-field content-input-field content-font block" placeholder="Содержимое" name="text"></textarea>
-                </p>
-                <input value="Опубликовать" class="button block" type="submit">
+            <form class="post-form" action="post_addition.php" method="post" enctype="multipart/form-data">
+                <div class="post-creating-part">
+                    <input class="post-input-field title-input-field title-font block" type="text" placeholder="Заголовок" name="title" maxlength="45"/>
+                    <p>
+                        <textarea class="post-input-field content-input-field content-font block" placeholder="Содержимое" name="text"></textarea>
+                    </p>
+                </div>
+                <div class="post-creating-part">
 
+                </div>
+                <div class="post-creating-part">
+                    <input value="Опубликовать" class="button " type="submit">
+                </div>
+                <div class="post-creating-part">
+                    <input name="image" id="upload-image" type="file" hidden="hidden">
+                    <input type="button" class="button upload-image-button" value="Загрузить изображение">
+                    <span class="upload-text" id="upload-text">Файл не выбран</span>
+                </div>
             </form>
         </li>
         <?php foreach (GetPosts($conn, $searchString) as $row){ ?>
             <li class="post-container">
                 <h2 class="title title-font"><?php echo highlight($row["Title"], $searchString) ?></h2>
-                <p class="content content-font"><?php echo highlight($row["Text"], $searchString) ?></p>
+                <p class="                                               content content-font"><?php echo highlight($row["Text"], $searchString) ?></p>
             </li>
         <?php }?>
     </ul>
+    <script src="js/script.js"></script>
 </main>
